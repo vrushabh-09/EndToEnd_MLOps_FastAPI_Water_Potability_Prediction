96fa2c080551aae4dad7579d55f7d4b893fceba3
vrushabh-09 <vrushabhpatil97711@gmail.com>
2026-04-11 19:35:19 +0530

    Added Cookie Cutter Template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine Learning System using FastAPI for Water Potability Prediction',
+    author='Vrushabh Patil',
+    license='MIT',
+)
